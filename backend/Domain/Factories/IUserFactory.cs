50720ad5251352db2bdce96563efb76b1c090091
sleepergoose <sleepergoose@gmail.com>
2024-06-17 00:03:23 +0300

    Add IUserRepository and UserFactory

---
+++ backend/Domain/Factories/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using Domain.ValueObjects.User;
+
+namespace Domain.Factories;
+
+public interface IUserFactory
+{
+    User Create(UserId id, UserName name, UserEmail email, UserPasswordHash passwordHash,Role role);
+}
---
+++ backend/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects.User;
+
+namespace Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetAsync(UserId id);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(User user);
+}
